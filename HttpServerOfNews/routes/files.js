b5bc5c44877f8960260636f8b800077a42d9ac2f
frosty <3313773846@qq.com>
2025-01-16 14:56:24 +0800

    feat: user page

--- HttpServerOfNews/routes/files.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const fileController = require('../controllers/fileController');
-
-router.post('/upload', fileController.upload);
-
-module.exports = router;
--- entry/.preview/default/cache/default/default@PreviewArkTS/esmodule/debug/entry/src/main/ets/common/utils/ToastUtil.ts
+++
@@ -1,8 +0,0 @@
-import promptAction from "@ohos:promptAction";
-import Constants from "@bundle:com.example.newsrelease/entry/ets/common/constants/Constants";
-export function showToast(msg: string | Resource) {
-    promptAction.showToast({
-        message: msg,
-        duration: Constants.ANIMATION_DURATION
-    });
-}
